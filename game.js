5f2477d6a0c80433fd69db89969343e9bc9cc39c
Leandro Parrado <lparrdev@gmail.com>
2022-04-15 09:46:03 -0400

    Linked game.js to index.html, error with reading 'text'

--- game.js
+++ game.js
@@ -4,12 +4,37 @@ const optionButtonsElement = document.getElementById('option-buttons')
 let state = {}
 
 function startGame() {
-
+    state = {}
+    showTextNode()
 }
 
 function showTextNode(textNodeIndex) {
-
+    const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
+    // Sets inner text to whatever is being displayed in textNodes
+    textElement.innerText = textNode.text
 }
+
+const textNodes = [
+    {
+        id: 1,
+        text: 'You wake up in a strange place and ahead of you lies a path.',
+        options: [
+            {
+                text: 'Move forward',
+                nextText: 2
+            },
+            {
+                text: 'Take item',
+                setState: { item: true },
+                nextText: 2
+            },
+            {
+                text: 'Move right',
+                nextText: 2
+            }
+        ]
+    }
+]
 
 function selectOption(option) {
 
