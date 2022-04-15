dace79b07fce4e4224d42be42cb0fb7f9cb5991e
Leandro Parrado <lparrdev@gmail.com>
2022-04-15 09:32:26 -0400

    Created pseudofunctions in game.js

--- game.js
+++ game.js
@@ -1,2 +1,18 @@
 const textElement = document.getElementById('text')
 const optionButtonsElement = document.getElementById('option-buttons')
+
+let state = {}
+
+function startGame() {
+
+}
+
+function showTextNode(textNodeIndex) {
+
+}
+
+function selectOption(option) {
+
+}
+
+startGame()
