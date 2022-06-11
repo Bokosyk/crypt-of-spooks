6bbd22cf1fe91cdf04688522ebbb1261b30fb572
Leandro Parrado <lparrdev@gmail.com>
2022-06-10 23:55:17 -0400

    localStorage to save theme switching

--- script.js
+++ script.js
@@ -11,6 +11,17 @@ const textElement = document.getElementById('text')
 const optionButtonsElement = document.getElementById('option-buttons')
 const textImage = document.getElementById('image')
 
+// Checks localStorage for theme
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme)
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        darkMode();
+    }
+}
+
 let state = {}
 
 function hide(item) {
@@ -46,9 +57,11 @@ function lightMode() {
 function switchTheme(event) {
     if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark')
+        localStorage.setItem('theme', 'dark')
         darkMode();
     } else {
         document.documentElement.setAttribute('data-theme', 'light')
+        localStorage.setItem('theme', 'light')
         lightMode();
     }
 }
